a5068131a90d7b96de898dc708d9cede1cd438b5
f-erm <f-erm@users.noreply.github.com>
2020-08-27 14:37:10 +0200

    Ab hier beginnt das Top experiment

--- main.py
+++ main.py
@@ -1,70 +1,22 @@
 import tkinter as tk
 from tkinter import ttk
+from ScrollableFrame import ScrollableFrame
 import os
+import shutil
+import sqlite3
 
 
 #TODO
-#Alles auf sqlite3 umstellen. Load so machen, dass alle erstmal geladen werden, und wenn sie die referenz auf ihre kinder suchen, scaun sie einfach
-#mittels id im dict nach -> alles kann in beleibiger reihenfolge geladen werden und verknüft sich automatisch
+#add subaim aufteilen, sodass top als id oder ref gegeben werden kann, und einträge nicht dupliziert werden, da add_subbutton ja auch zu sql hinzufügt
+#löschen möglich machen
+#top wird nur gebraucht um das neue aim anzuhängen....
 #jetzt da aims über id zugänglich sind müssen die tops nicht mehr immer ls referenz mitangegeben werden, oder? zumindest überprüfen bitte
 #switchsite und loadsite irgendwie besser machen
-#scrollable frame in seperate klasse auslagern
-###################################
 #die ganzen loadsite referenzen, die den frame immer als argument mitgeben entfernen und direkt den ensprechenden frame geben
 #alles optisch hübscher machen/stabilität testen
-#vlt dassubaim hinzufügen in aimbutton class machen, dann muss nicht immer top mitangegeben werden
-#packroutine rekursiv machen
 
 
-class ScrollableFrame(ttk.Frame):
-    #frame with scrollbars.Can can be vertical or both
-    #use self.scrollable_frame to acces content of frame
-    def __init__(self, container, orientation ="vertical", *args, **kwargs):
-        super().__init__(container, *args, **kwargs)
-        self.canvas = tk.Canvas(self)
-        self.canvas.configure(width=250,height = 250)
-        if orientation == "vertical":
-            scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-            self.scrollable_frame = ttk.Frame(self.canvas)
-            self.scrollable_frame.bind(
-                "<Configure>",
-                lambda e: self.canvas.configure(
-                    scrollregion=self.canvas.bbox("all")))
-            self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-            self.canvas.configure(yscrollcommand=scrollbar.set)
-            self.canvas.pack(side="left", fill="both", expand=True)
-            scrollbar.pack(side="right", fill="y")
-        elif orientation == "horizontal":
-            scrollbar = ttk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
-            self.scrollable_frame = ttk.Frame(self.canvas)
-            self.scrollable_frame.bind(
-                "<Configure>",
-                lambda e: self.canvas.configure(
-                    scrollregion=self.canvas.bbox("all")))
-            self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-            self.canvas.configure(xscrollcommand=scrollbar.set)
-            self.canvas.pack(side="top",fill="both",expand=True)
-            scrollbar.pack(side="bottom", fill="x")
-        elif orientation == "both":
-            vertbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-            horbar = ttk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
-            self.scrollable_frame = ttk.Frame(self.canvas)
-            self.scrollable_frame.bind(
-                "<Configure>",
-                lambda e: self.canvas.configure(
-                    scrollregion=self.canvas.bbox("all")))
-            self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-            self.canvas.configure(xscrollcommand=horbar.set)
-            self.canvas.configure(yscrollcommand=vertbar.set)
-            vertbar.pack(side="right", fill="y")
-            self.canvas.pack(side="top",fill="both",expand=True)
-            horbar.pack(side="bottom", fill="x")
-        else:
-            raise("orientation required. Use vertical,horizontal,both")
-
-    def frame(self):
-        #for easier use
-        return self.scrollable_frame
+DATAFILE_NAME = "data.db"
 
 
 class aimbutton(ttk.Button):
@@ -73,7 +25,7 @@ class aimbutton(ttk.Button):
         super().__init__(container,*args,text=kwargs["text"],command=kwargs["command"])
         self.id=kwargs["id"]
         self.listname=kwargs["listname"]
-        self.top = kwargs["top"]
+        self.top = kwargs["top"] #Given as reference
         self.text=kwargs["text"]
         self.done = tk.BooleanVar()
         self.done.set(False)
@@ -81,7 +33,7 @@ class aimbutton(ttk.Button):
         self.link=None
 
     def packroutine(self,reihe=1):
-        #place the buttons on their destined position and check if link is done
+        '''place the buttons on their destined position and checks if link is done'''
         if(self.link != None):
             if(aims[self.link][0].done.get()):
                 self.done.set(True)
@@ -98,7 +50,7 @@ class aimbutton(ttk.Button):
             i.packroutine(reihe+1)
 
     def lineroutine(self):
-        #draws the lines
+        ''' Draws the lines for all subaims'''
         def drawline(end,done):
             if done:
                 fill='green'
@@ -124,7 +76,7 @@ class aimbutton(ttk.Button):
 
     def getselfinfo(self):
         #returns all relevant atributes, top and subs are given as id
-        #Order is listname,id,text,done,top,subs,link
+        #Order is listname,id,text,done,top,subs,link TODO rework pls
         if self.top==None:
             top="N"
         else:
@@ -140,35 +92,6 @@ class aimbutton(ttk.Button):
         return (self.listname,str(self.id),self.text,str(self.done.get())
         ,top,sub,link)
 
-
-
-def save(data,data_old):
-    #save current programm state into data.txt
-    print("save")
-    #try to open datafile
-    try:
-        with open(data,"r") as f:
-            backup = f.readlines()
-        f = open(data,mode="w")
-    except:
-        print("unable to open datafile")
-        return
-    #write to file
-    try:
-        for aim in aims.keys():
-            for a in aims[aim]:
-                f.write("".join([i+"||"for i in a.getselfinfo()]))
-                f.write("\n")
-            f.write("\n")
-        f.close()
-        #copy old content to data_old:
-        with open(data_old,"w") as ao:
-            ao.writelines(backup)
-    except:
-        print("unable to save, reverted changes")
-        f.close()
-        with open(data,"w") as f:
-            f.writelines(backup)
 
 
 def cancon(self=0):
@@ -192,22 +115,11 @@ def cancon(self=0):
     mainframe.bind("<Configure>",cancon)
 
 
-def get_all_ids():
-    #return list of all used ids TODO als List comprehenseion [i for i in usw] schreiben
-    l= []
-    for i in aims.keys():
-        for e in aims[i]:
-            l.append(e.id)
-    return l
-
-
 def get_new_id():
-    #get lowest availible id
-    id = 0
-    l= get_all_ids()
-    while id in l:
-        id += 1
-    return id
+    #get availible id
+    global highest_id
+    highest_id += 1
+    return highest_id
 
 
 def switchframes(old,new,destroy=False):
@@ -226,17 +138,16 @@ def destroy_all():
     #destroy the current state TODO auch automatisch den aktuellen frame löschen?
     global aims
     global aims_by_id
+    global conn
     aims = {}
     aims_by_id={}
     for button in sideframecontent.scrollable_frame.winfo_children():
         button.destroy()
+    conn.rollback()
 
-def resetsave(data,data_old):
+def resetsave(data,data_old): #TODO
     print("reset save")
-    #switch content of data and data_old
-    os.rename(data,data_old+"___tmp")
-    os.rename(data_old,data)
-    os.rename(data_old+"___tmp",data_old)
+    os.rename(DATAFILE_NAME+"_old",DATAFILE_NAME)
     destroy_all()
     #prepare site
     switchframes(settingsframe,mainframe,destroy=True)
@@ -245,46 +156,6 @@ def resetsave(data,data_old):
     load(data)
 
 
-#TODO
-#add(name,id)mact das top aim , sidebutton+ersten aimbutton
-#add_subaim() ist zum subs ädden
-def load(data):
-    print("load:")
-    print(data)
-    try:
-        with open(data,mode='r') as file:
-            #splitt into lists, containing only sub aims with one main aim
-            sections=[[]]
-            counter = 0
-            for line in file.read().splitlines():
-                if line == "":
-                    sections.append([])
-                    counter += 1
-                else:
-                    sections[counter].append(line)
-            if sections[-1]==[]:
-                del sections[-1]
-            #turn aims into usable format
-            sections=[[s.split("||")[:-1] for s in section] for section in sections]
-            #add the aims, starting with the top aim
-            for section in sections:
-                #add the aims, starting with the top aim
-                cur_aim = section.pop(0)
-                add_aim(cur_aim[0],int(cur_aim[1]))
-                #handle all its subs:
-                while(len(section)>0):
-                    for aim in list(section):
-                        if int(aim[4]) in get_all_ids():#wenn top schon existiert
-                            #create it, top is known -- listname,id,text,done,top,subs,link
-                            section.remove(aim)
-                            add_subaim(aim[0],int(aim[1]),aim[2],aims_by_id[int(aim[4])])
-    except:
-       destroy_all()
-       print("unable to load")
-    if len(aims)==0:
-        loadsite(None,titlevar,aimframecontent.frame())
-    else:
-        loadsite(list(aims.keys())[0],titlevar,aimframecontent.frame())
 
 def remove_aim(captvar):
     #invert remove and set catpvar accordingly
@@ -351,20 +222,35 @@ def mainclick(name,this):
             ,getlink(name,this)])
             remove_aim(remove_side_caption)
 
+'''
+def create_subaim(listname, id , text):
+    '''creates instance of new subaim, without linking it to different aim.
+    Top start out with None, needs to be set to the right value ASAP'''
+    new = aimbutton(aimframecontent.scrollable_frame,listname=listname
+    ,text=text,id=id,top=None,command=lambda:mainclick(listname,new))
+    aims[listname].append(new)
+    aims_by_id[id]=new
+'''
 
 def add_subaim(listname,id,text,top):
-    #make new subaim of top and append it. vlt lieber als func von aimbutton class?
+    #make new subaim of top(given by ref) and append it
+    global conn
     new = aimbutton(aimframecontent.scrollable_frame,listname=listname
     ,text=text,id=id,top=top,command=lambda:mainclick(listname,new))
     aims[listname].append(new)
     aims_by_id[id]=new
     top.subs.append(new)
-    return new #for manuall adding of subaims
+    cur = conn.cursor()
+    cur.execute(
+    f'INSERT into aims Values({str(id)}, "{listname}", "{text}", 0, {str(top.id)}, NULL);')
+    cur.execute(
+    f'INSERT into subs VALUES({str(top.id)}, {str(id)}, "{listname}");')
 
 
 def add_aim(name,id):
     #add the new main aim. creates the nessecary sidebutton and the first
     #aimbutton. only needs listname and id to do this
+    global conn
     if name not in aims.keys():
         aims[name]=[]
         #make new aimbutton
@@ -373,11 +259,12 @@ def add_aim(name,id):
         ,command=lambda:mainclick(name,None))
         aims[name].append(new)
         aims_by_id[id]=new
+        #add to database
+        cur = conn.cursor().execute(f'INSERT into aims Values({str(id)}, "{name}", "{name}", 0, NULL, NULL)')
         #add the nessecary sidebutton
         ttk.Button(sideframecontent.scrollable_frame
         ,text = name, command=lambda: sideclick(name)).pack()
         loadsite(name,titlevar,aimframe)
-        return new#for adding aims manually
 
 
 def sideclick(name):
@@ -417,15 +304,71 @@ def loadsite(name,caption,aimframe):
         caption.set("Aktuelle Auswahl:     ")
 
 
+def establish_sub(top, sub):
+    '''creates link between top and sub, both given as id'''
+    aims_by_id[top].subs.append(aims_by_id[sub])
+    aims_by_id[sub].top = aims_by_id[top]
+
+
+def load(datafile_name):
+    '''loads the data from datafile_name, sets up all the aims and returns the
+    sql connection'''
+    #try:
+    global conn
+    conn = sqlite3.connect(DATAFILE_NAME)
+    cur = conn.cursor()
+    cur.execute(
+    'create table if not exists \
+    aims(id integer,listname text, name text, done integer, top integer, \
+    link text);')
+    cur.execute(
+    'create table if not exists subs(id integer, child_id integer, list text);')
+    #load the main aims
+    cur.execute('SELECT listname, id FROM aims WHERE top IS NULL;')
+    for row in cur:
+        print(row)
+        add_aim(row[0],row[1])
+    #load the subaims listname,id,text,top
+    cur.execute('SELECT listname, id, name, top FROM aims WHERE top IS NOT NULL;')
+    for row in cur:
+        print(row)
+        add_subaim(row[0], row[1], row[2], row[3])
+    #load the connections between the aims
+    cur.execute('SELECT * FROM subs;')
+    for row in cur:
+        establish_sub(row[0],row[1])
+    #Get highest id
+    cur.execute('SELECT MAX(id) FROM aims')
+    global highest_id
+    try:
+        highest_id = cur.fetchall()[0][0]
+    except:
+        highest_id = 0
+    #except:
+    #    print("unable to open/load datafile, shutting down...")
+    #    exit()
+
+
+def save(conn,datafile_name):
+    '''commit changes and preserve copy of datafile'''
+    try:
+        shutil.copyfile(
+            os.path.join(os.getcwd(),DATAFILE_NAME)
+            ,os.path.join(os.getcwd()+DATAFILE_NAME+"_old"))
+        conn.commit()
+        print("saved successfully")
+    except:
+        print("unable to save!!!")
+
 
 #Globals
 aims={}
 aims_by_id={}
 global remove
+global conn
+highest_id
 remove = False
 reihenbreite={}
-data_location="data.txt"
-data_old_location="data_old.txt"
 
 #setup frames
 root = tk.Tk()
@@ -484,7 +427,7 @@ settings_button=ttk.Button(manage_frame,text="Settings"
 settings_button.pack(side="right")
 
 savebutton=ttk.Button(manage_frame,text="Save"
-,command=lambda:save(data_location,data_old_location))
+,command=lambda:save(conn,DATAFILE_NAME))
 savebutton.pack(side="right",padx=(10,30))
 
 add_side=ttk.Button(manage_aims_frame,text="Add Aim"
@@ -522,7 +465,7 @@ def get_side_add_button(aimframe,settingsframe,mainframe,dict,caption):
     return frame
 
 def create_subbuttons(listname,top):
-    #load options to create new subaim of an aim given by id and listname
+    #load options to create new subaim of an aim given by ref and listname
     #setup
     frame = tk.Frame(settingsframe,highlightbackground="black", highlightthickness=1)
     label = ttk.Label(frame,text="Name of subname:")
@@ -613,9 +556,9 @@ def get_revert():
 ################################################################################
 
 #fill with testcontent
+load(DATAFILE_NAME)
 add_aim("lol",100)
 add_aim("hallo",101)
-load(data_location)
 
 #setup resize events
 mainframe.bind("<Configure>",cancon)
